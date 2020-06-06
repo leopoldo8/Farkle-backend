8c43ef943e81dbefe16a3e6420352238ac36e0dc
Rodrigo Haertel <rodrigo.haertel@yahoo.com.br>
2020-06-06 11:52:51 -0300

    FEAT: secret keys env

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,11 +1,10 @@
 import * as mongoose from 'mongoose';
-import { mongodbURL } from '@common/secret';
 
 export const databaseProviders = [
   {
     provide: 'DATABASE_CONNECTION',
     useFactory: async (): Promise<typeof mongoose> =>
-      await mongoose.connect(mongodbURL, {
+      await mongoose.connect(process.env.DATABASE_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         dbName: 'app',
